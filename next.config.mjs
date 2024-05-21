434abda3ca568962f1920c34289dcdb68a331ef1
azishio <azishio3141@gmail.com>
2024-05-22 07:15:42 +0900

    🔧 ウェブアセンブリを動かすための設定

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+	transpilePackages: ["@mui/x-charts"],
+	webpack(config, { isServer, dev }) {
+		config.output.webassemblyModuleFilename =
+			isServer && !dev ? "../static/wasm/wasm.wasm" : "static/wasm/wasm.wasm";
+
+		config.experiments = { ...config.experiments, syncWebAssembly: true };
+
+		return config;
+	},
+};
 
 export default nextConfig;
